2d7361ee2359135932873e0e386b0aa535ca96ff
Kateryna58 <stadnyk_ak16@nuwm.edu.ua>
2020-12-10 01:22:43 +0200

    create rundom massive

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const D=7;
-const RHO=0.4;
-const BETA=0.9;
-let vector_1=new FeatureVector([1,0,1,0,1,1,0]);
-let vector_2=new FeatureVector([1,0,0,1,0,1,1]);
-let vector_3=new FeatureVector([1,0,1,0,1,0,0]);
-let vector_4=new FeatureVector([1,0,1,0,0,0,1]);
-let vector_5=new FeatureVector([0,1,0,0,1,0,0]);
-let vector_6=new FeatureVector([0,0,1,0,0,1,0]);
-let vector_7=new FeatureVector([0,0,1,0,0,0,1]);
-let vector_8=new FeatureVector([0,1,0,0,1,0,1]);
-let vector_9=new FeatureVector([1,1,1,0,0,1,0]);
+const RHO = 0.4;
+const BETA = 0.9;
+let y,
+  P = new Array();
+let D = document.querySelector("#D").value;
+let N = document.querySelector("#N").value;
+function generateRandomArray() {
+  for (let i = 0; i < N; i++) {
+    P[i] = new Array();
+    for (let j = 0; j < D; j++) {
+      y = Math.random();
+      if (y < 0.5) {
+        P[i][j] = Math.floor(y);
+      } else {
+        P[i][j] = Math.ceil(y);
+      }
+    }
+  }
+  console.log(P);
+}
